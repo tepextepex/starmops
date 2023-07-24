01e76579c97e88e68f8a29a339f44caf90766481
tepextepex <tepextepex@gmail.com>
2023-07-24 12:45:44 +0300

    Fixed hero names

--- gui/selection_screen.py
+++ gui/selection_screen.py
@@ -16,7 +16,6 @@ class SelectionScreen:
         for i, a in enumerate(chars):
             x = 95 + 100 * i + 33
             y = 190
-            screen.draw.text(a.name, midtop=(x, y + 10))
             a.actor.midbottom = x, y
 
         self.desc_text = "Select your party. You can choose three members"
@@ -25,6 +24,9 @@ class SelectionScreen:
     def render(self, party):
         for c in self.chars:
             c.actor.draw()
+            p = c.actor.midbottom
+            self.screen.draw.text(c.name, midtop=(p[0], p[1] + 10))
+
         self.screen.draw.text(self.desc_text,
                               center=(self.screen.width / 2, self.screen.height / 2 + 80),
                               width=500)
