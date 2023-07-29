2480e37f3707d30056a0d07671129bc391b3fb1e
tepextepex <tepextepex@gmail.com>
2023-07-29 14:11:59 +0300

    Added semi-transparent fill to the Panels

--- gui/base.py
+++ gui/base.py
@@ -1,7 +1,8 @@
+import pygame
 from pygame import Rect
 from pgzero.actor import Actor
 
-c_white = (255, 255, 255, 128)
+c_white = (255, 255, 255, 64)
 c_red = (255, 87, 51)
 c_green = (76, 187, 23)
 c_blue = (117, 194, 246)
@@ -17,7 +18,7 @@ class NextBtn:
 
 
 class Panel:
-    def __init__(self, screen, padding, x, y, w, h, active=False):
+    def __init__(self, screen, padding, x, y, w, h, active=False, fill=False):
         self.screen = screen
         self.padding = padding
         if active:
@@ -26,6 +27,8 @@ class Panel:
         else:
             self.c = c_white
             self.active = False
+
+        self.fill = fill
 
         self.x = x
         self.y = y
@@ -55,4 +58,9 @@ class Panel:
 
     def render(self):
         self.box = Rect((self.x, self.y), (self.w, self.h))
+        if self.fill:
+            cover = pygame.Surface((self.w, self.h))
+            cover.set_alpha(40)
+            cover.fill(self.c)
+            self.screen.blit(cover, (self.x, self.y))
         self.screen.draw.rect(self.box, self.c)
